0b40503ced245280d74b488e4c4bae0e79e72791
Eric Poppenheimer <31865490+ecpoppenheimer@users.noreply.github.com>
2022-05-12 10:14:15 -0700

    Update tfquaternion/tfquaternion.py
    Removed extra whitespace.
    
    Co-authored-by: Phil <ijund.phil@gmail.com>

--- tfquaternion/tfquaternion.py
+++ tfquaternion/tfquaternion.py
@@ -186,7 +186,7 @@ def quaternion_between_3d(u, v, epsilon=1e-6, dtype=tf.float32):
     cross = tf.linalg.cross(u, v)
 
     # But if they rotation is a pure 180 flip then this will cause the dot product to become -1
-    # which will create a zero quaternion, which is not what we want.  So have to manually construct
+    # which will create a zero quaternion, which is not what we want. So have to manually construct
     # the quaternion in this case
     if dot < epsilion:
 	    # need any vector orthogonal to the input.  Try crossing with the x-axis and if that does not
